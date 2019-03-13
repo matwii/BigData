8624c70fcfb283fb5bf45f3d660d9ebb2433e330
Matz Wiik <matzwiik@gmail.com>
2019-03-13 14:57:58 +0100

    Task 8 done

--- ex8.py
+++ ex8.py
@@ -10,7 +10,16 @@ albums = sc.textFile("./albums.csv")
 artists = sc.textFile("./artists.csv")
 
 rdd_albums = albums.map(lambda x: x.split(','))
-top_rated_albums = rdd_albums.filter(lambda x: float(x[8])==5.0).map(lambda x: int(x[1]))
+rdd_artists = artists.map(lambda x: x.split(','))
 
-print(top_rated_albums.collect())
+filtered_albums = (rdd_albums.filter(lambda x: float(x[8])==5.0).map(lambda x: x[1]).collect())
+filtered_artists = (rdd_artists.filter(lambda x: x[0] in filtered_albums).map(lambda x: x[2]).sortBy(lambda x: x).distinct().collect())
+
+filtered_artists = list(filter(None, filtered_artists))
+
+sc.parallelize(filtered_artists).map(lambda x: ''.join([str(word) for word in x]))\
+    .saveAsTextFile('results/result_8.tsv')
+
+print(filtered_artists)
+
 
